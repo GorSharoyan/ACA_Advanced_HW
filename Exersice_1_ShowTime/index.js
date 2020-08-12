2da0385a9d2d63490323eeb181921021b7dde8f6
GorSharoyan <gor.sharoyan95@gmail.com>
2020-08-13 01:27:22 +0400

    Delete index.js

--- Exersice_1_ShowTime/index.js
+++
@@ -1,6 +0,0 @@
-let ShowTimeBtn=document.querySelector('.show-time')
-let ShowTime=document.querySelector('.time')
-
-ShowTimeBtn.addEventListener('click',()=>{
-    ShowTime.innerHTML=Date();
-   })
